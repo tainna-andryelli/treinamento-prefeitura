9061a256b5cd0ab8c45a66474a87294e308f2209
Tainna <tainnaandryelli@gmail.com>
2024-03-28 11:17:54 -0300

    feat: Table and People Controller

---
+++ database/migrations/2024_03_28_130641_create_people_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255)->nullable(false);
+            $table->date('date_birth')->nullable(false);
+            $table->string('cpf', 11)->unique()->nullable(false);
+            $table->enum('sex', ['M', 'F', 'Outro'])->nullable(false);
+            $table->string('city')->nullable();
+            $table->string('neighborhood')->nullable();
+            $table->string('street')->nullable();
+            $table->string('number')->nullable();
+            $table->string('complement')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('people');
+    }
+};
